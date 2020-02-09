3b0171ebbf056b0b59106fe0f9ec28e25ed6f16c
john-emerson14 <john.carv.sousa@gmail.com>
2020-02-09 14:56:42 -0300

    feat: add is_jump_interrupted

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -4,7 +4,7 @@ extends Actor
 func _physics_process(delta: float) -> void:
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
-	_velocity = calculate_move_velocity(_velocity, direction, speed)
+	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
 	var snap: Vector2 = Vector2.DOWN * 60.0 if direction.y == 0.0 else Vector2.ZERO
 	_velocity = move_and_slide_with_snap(
 		_velocity, snap, FLOOR_NORMAL, true
@@ -21,10 +21,13 @@ func get_direction() -> Vector2:
 func calculate_move_velocity(
 		linear_velocity: Vector2,
 		direction: Vector2,
-		speed: Vector2
+		speed: Vector2,
+		is_jump_interrupted: bool
 	) -> Vector2:
 	var velocity: = linear_velocity
 	velocity.x = speed.x * direction.x
 	if direction.y != 0.0:
 		velocity.y = speed.y * direction.y
+	if is_jump_interrupted:
+		velocity.y = 0.0
 	return velocity
