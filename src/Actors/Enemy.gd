01ca9fe0785755c2a7f53a2919e9955b459a4a09
john-emerson14 <john.carv.sousa@gmail.com>
2020-02-13 22:10:55 -0300

    feat: add stomp impulse

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -9,6 +9,7 @@ func _ready() -> void:
 func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
 	if body.global_position.y > get_node("StompDetector").global_position.y:
 		return
+	get_node("CollisionShape2D").disabled = true
 	queue_free()
 
 
--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,6 +1,13 @@
 extends Actor
 
 
+export var stomp_impulse: = 1000.0
+
+
+func _on_EnemyDetector_area_entered(area: Area2D) -> void:
+	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
+
+
 func _physics_process(delta: float) -> void:
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
@@ -31,3 +38,9 @@ func calculate_move_velocity(
 	if is_jump_interrupted:
 		velocity.y = 0.0
 	return velocity
+	
+	
+func calculate_stomp_velocity(linear_velocity: Vector2, impulse: float) -> Vector2:
+	var out: = linear_velocity
+	out.y = -impulse
+	return out
