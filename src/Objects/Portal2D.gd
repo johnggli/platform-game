825a9aba7152437416bf673e79b4d14c3e9dbd23
john-emerson14 <john.carv.sousa@gmail.com>
2020-02-20 21:43:06 -0300

    feat: add script to portal2D

---
+++ src/Objects/Portal2D.gd
@@ -0,0 +1,19 @@
+tool
+extends Area2D
+
+
+#onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
+onready var anim_player: AnimationPlayer = $AnimationPlayer
+
+
+export var next_scene: PackedScene
+
+
+func _get_configuration_warning() -> String:
+	return "the next scene property can't be empty" if not next_scene else ""
+
+
+func teleport() -> void:
+	anim_player.play("fade_in")
+	yield(anim_player, "animation_finished")
+	get_tree().change_scene_to(next_scene)
