4ae4d34ac7c620f2b85eb2a06f2f17dc844d94e3
john-emerson14 <john.carv.sousa@gmail.com>
2020-02-20 21:58:04 -0300

    feat: add level02

--- src/Objects/Portal2D.gd
+++ src/Objects/Portal2D.gd
@@ -9,6 +9,10 @@ onready var anim_player: AnimationPlayer = $AnimationPlayer
 export var next_scene: PackedScene
 
 
+func _on_body_entered(body: PhysicsBody2D) -> void:
+	teleport()
+
+
 func _get_configuration_warning() -> String:
 	return "the next scene property can't be empty" if not next_scene else ""
 
