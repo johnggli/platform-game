8360cca3974b84c01a9620f3487864f1f398486b
john-emerson14 <john.carv.sousa@gmail.com>
2020-02-22 20:07:51 -0300

    feat: add autoload PlayerData

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -1,6 +1,9 @@
 extends "res://src/Actors/Actor.gd"
 
 
+export var score: = 100
+
+
 func _ready() -> void:
 	set_physics_process(false)
 	_velocity.x = -speed.x
@@ -10,7 +13,7 @@ func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
 	if body.global_position.y > get_node("StompDetector").global_position.y:
 		return
 	get_node("CollisionShape2D").disabled = true
-	queue_free()
+	die()
 
 
 func _physics_process(delta: float) -> void:
@@ -18,3 +21,8 @@ func _physics_process(delta: float) -> void:
 	if is_on_wall():
 		_velocity.x *= -1.0
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
+	
+
+func die() -> void:
+	PlayerData.score += score
+	queue_free()
--- src/Objects/Coin.gd
+++ src/Objects/Coin.gd
@@ -4,7 +4,13 @@ extends Area2D
 onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
 
 
+export var score: = 100
 
 
 func _on_body_entered(body: PhysicsBody2D) -> void:
-	anim_player.play("fade_out")
+	picked()
+	
+	
+func picked() -> void:
+	PlayerData.score += score
+	anim_player.play("picked")
