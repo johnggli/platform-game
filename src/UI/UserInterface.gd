3c13b8abe5d1d3ddab52656cc9e1d66a416ccab3
john-emerson14 <john.carv.sousa@gmail.com>
2020-02-23 10:55:56 -0300

    feat: connect PlayerData to UserInterface

--- src/UI/UserInterface.gd
+++ src/UI/UserInterface.gd
@@ -3,14 +3,25 @@ extends Control
 
 onready var scene_tree: = get_tree()
 onready var pause_overlay: ColorRect = get_node("PauseOverlay")
+onready var score: Label = get_node("Label")
 
 var paused: = false setget set_paused
+
+
+func _ready() -> void:
+	PlayerData.connect("score_updated", self, "update_interface")
+	PlayerData.connect("player_died", self, "_PlayerData_player_died")
+	update_interface()
 
 
 func _unhandled_input(event: InputEvent) -> void:
 		if event.is_action_pressed("pause"):
 			self.paused = not paused
 			scene_tree.set_input_as_handled()
+
+
+func update_interface() -> void:
+	score.text = "Score: %s" % PlayerData.score
 
 
 func set_paused(value: bool) -> void:
